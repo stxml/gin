6c6c5bec975db7d5eaa4dd5bb2a72040bf2f2ba3
stxml <stxml@users.noreply.github.com>
2016-07-15 22:56:02 +0200

    added and refactored tests for Form binding

--- binding/binding_test.go
+++ binding/binding_test.go
@@ -81,6 +81,33 @@ func TestBindingForm2(t *testing.T) {
 		"", "")
 }
 
+func TestBindingFormWithPointers1(t *testing.T) {
+	testFormBindingWithPointers(t, "GET", "/?Int=1&Int8=1&Int16=1&Int32=1&Int64=1&UInt=1&UInt8=1&UInt16=1&UInt32=1&UInt64=1&Bool=true&Float32=0.1&Float64=0.1&String=foo", "")
+}
+
+func TestBindingFormWithPointers2(t *testing.T) {
+	testFormBindingWithPointers(t, "POST", "/", "Int=1&Int8=1&Int16=1&Int32=1&Int64=1&UInt=1&UInt8=1&UInt16=1&UInt32=1&UInt64=1&Bool=true&Float32=0.1&Float64=0.1&String=foo")
+}
+
+func TestFormBindingMixed(t *testing.T) {
+	var obj struct {
+		Foo *int   `form:"foo"`
+		Bar string `form:"bar"`
+	}
+	req := requestWithBody("GET", "/?foo=1&bar=yes", "")
+	err := Form.Bind(req, &obj)
+	assert.NoError(t, err)
+}
+
+func TestFormBindingWithPointersFails(t *testing.T) {
+	var obj struct {
+		Foo *int `form:"foo"`
+	}
+	req := requestWithBody("GET", "/?foo=bar", "")
+	err := Form.Bind(req, &obj)
+	assert.Error(t, err)
+}
+
 func TestBindingXML(t *testing.T) {
 	testBodyBinding(t,
 		XML, "xml",
@@ -167,6 +194,18 @@ func TestExistsSucceeds(t *testing.T) {
 	assert.NoError(t, err)
 }
 
+func TestExistsWithFormSucceeds(t *testing.T) {
+	type HogeStruct struct {
+		Hoge *int `form:"hoge" binding:"exists"`
+	}
+
+	var obj HogeStruct
+	req := requestWithBody("POST", "/", "hoge=0")
+	req.Header.Add("Content-Type", MIMEPOSTForm)
+	err := Form.Bind(req, &obj)
+	assert.NoError(t, err)
+}
+
 func TestExistsFails(t *testing.T) {
 	type HogeStruct struct {
 		Hoge *int `json:"foo" binding:"exists"`
@@ -175,6 +214,18 @@ func TestExistsFails(t *testing.T) {
 	var obj HogeStruct
 	req := requestWithBody("POST", "/", `{"boen": 0}`)
 	err := JSON.Bind(req, &obj)
+	assert.Error(t, err)
+}
+
+func TestExistsWithFormFails(t *testing.T) {
+	type HogeStruct struct {
+		Hoge *int `form:"foo" binding:"exists"`
+	}
+
+	var obj HogeStruct
+	req := requestWithBody("POST", "/", `boen=0`)
+	req.Header.Add("Content-Type", MIMEPOSTForm)
+	err := Form.Bind(req, &obj)
 	assert.Error(t, err)
 }
 
@@ -198,34 +249,15 @@ func testFormBinding(t *testing.T, method, path, badPath, body, badBody string) 
 	assert.Error(t, err)
 }
 
-func TestFormBindingWithPointers1(t *testing.T) {
+func testFormBindingWithPointers(t *testing.T, method, path, body string) {
+	req := requestWithBody(method, path, body)
+	if method == "POST" {
+		req.Header.Add("Content-Type", MIMEPOSTForm)
+	}
+
 	obj := StructWithPointers{}
-	req := requestWithBody("GET", "/?Int=1&Int8=1&Int16=1&Int32=1&Int64=1&UInt=1&UInt8=1&UInt16=1&UInt32=1&UInt64=1&Bool=true&Float32=0.1&Float64=0.1&String=foo", "")
-
 	err := Form.Bind(req, &obj)
-	assert.NoError(t, err)
-	assert.EqualValues(t, *obj.Int, 1)
-	assert.EqualValues(t, *obj.Int8, 1)
-	assert.EqualValues(t, *obj.Int16, 1)
-	assert.EqualValues(t, *obj.Int32, 1)
-	assert.EqualValues(t, *obj.Int64, 1)
-	assert.EqualValues(t, *obj.UInt, 1)
-	assert.EqualValues(t, *obj.UInt8, 1)
-	assert.EqualValues(t, *obj.UInt16, 1)
-	assert.EqualValues(t, *obj.UInt32, 1)
-	assert.EqualValues(t, *obj.UInt64, 1)
-	assert.EqualValues(t, *obj.Bool, true)
-	assert.EqualValues(t, *obj.Float32, float32(0.1))
-	assert.EqualValues(t, *obj.Float64, 0.1)
-	assert.EqualValues(t, *obj.String, "foo")
-}
 
-func TestFormBindingWithPointers2(t *testing.T) {
-	obj := StructWithPointers{}
-	req := requestWithBody("POST", "/", "Int=1&Int8=1&Int16=1&Int32=1&Int64=1&UInt=1&UInt8=1&UInt16=1&UInt32=1&UInt64=1&Bool=true&Float32=0.1&Float64=0.1&String=foo")
-	req.Header.Add("Content-Type", MIMEPOSTForm)
-
-	err := Form.Bind(req, &obj)
 	assert.NoError(t, err)
 	assert.EqualValues(t, *obj.Int, 1)
 	assert.EqualValues(t, *obj.Int8, 1)
